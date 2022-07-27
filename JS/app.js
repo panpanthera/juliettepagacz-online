694fef28c882e42bf62c83bd6ec0bf832efd7f77
Juliette Pagacz <jpagacz@student.s19.be>
2022-07-27 18:36:00 +0200

    All the necessary js

---
+++ JS/app.js
@@ -0,0 +1,98 @@
+/*
+*Anime un titre avec un effet d'apparition mot par mot
+*
+*@param {string} selector
+*/
+function animateTitle(selector) {
+    const title = document.querySelector(selector)
+    if (title == null) {
+        console.error('impossible de trouver l\'élément' + selector)
+        return
+    }
+
+
+
+    const spans = spanify(title)
+    title.style.opacity = 1
+    console.log('spans', spans)
+
+    spans.forEach((span, k) => {
+        span.children[0].style.animationDelay = (k * .1) + 's'
+    })
+}
+
+/**
+ * Entoure chaque mot d'un span
+ * 
+ * @param {Node} element 
+ * @returns {HTMLSpanElement[]}
+ */
+
+function spanify(element) {
+    // on construiut iun tab contenant la nouvelle structure
+    const children = Array.from(element.childNodes)
+    let spans = []
+    let elements = []
+    children.forEach(child => {
+        if (child.nodeType == Node.TEXT_NODE) {
+            const words = child.textContent.split(' ')
+            console.log(words)
+            let wordSpans = words.map(wrapWord)
+            spans = spans.concat(wordSpans)
+            elements = elements.concat(
+                injectElementBetweenItems(wordSpans, document.createTextNode(' '))
+            )
+        } else if (child.tagName == 'BR') {
+            elements.push(child)
+        } else {
+            spans = spans.concat(spanify(child))
+            elements.push(child)
+        }
+    })
+    console.log(elements)
+
+    // puis on utilise ce tableau et on injecte les élémens dans le title
+    element.innerHTML = ''
+    elements.forEach(el => {
+        element.appendChild(el)
+    })
+    return spans
+}
+
+/**
+ * 
+ * Entoure le mot de deux spans
+ * 
+ * 
+ * @param {string} word
+ * 
+ */
+
+function wrapWord(word) {
+    const span = document.createElement('span')
+    const span2 = document.createElement('span')
+    span.appendChild(span2)
+    span2.innerHTML = word
+    return span
+}
+
+/**
+ * @param {Node[]} arr
+ * @param {Node} element Element à injecter entre chaque élement du tableau
+ * @return {Node[]}
+ */
+
+function injectElementBetweenItems(arr, element) {
+    return arr.map((item, k) => {
+        if (k == arr.length - 1) {
+            return [item]
+        }
+        return [item, element.cloneNode()]
+    }).reduce((acc, pair) => {
+        acc = acc.concat(pair)
+        return acc
+    }, [])
+
+}
+
+animateTitle('.title')
---
+++ JS/locomotive.js
@@ -0,0 +1,125 @@
+/*import LocomotiveScroll from 'locomotive-scroll';*/
+gsap.registerPlugin(ScrollTrigger);
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector("[data-scroll-container]"),
+  smooth: true,
+  tablet: {
+    smooth: true
+  },
+  smartphone: {
+    smooth: true
+  },
+  repeat: true,
+});
+
+locoScroll.on("scroll", ScrollTrigger.update);
+
+ScrollTrigger.scrollerProxy("[data-scroll-container]", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+  getBoundingClientRect() {
+    return {
+      top: 0,
+      left: 0,
+      width: window.innerWidth,
+      height: window.innerHeight
+    };
+  },
+  pinType: document.querySelector("[data-scroll-container]").style.transform ? "transform" : "fixed"
+});
+
+
+
+/*ScrollTrigger.defaults({
+   toggleActions: "restart pause reverse pause"
+});*/
+/*
+gsap.to(".purple", {
+  scrollTrigger: {
+    trigger: ".purple",
+    toggleActions: "restart pause reverse pause",
+    scroller: "[data-scroll-container]"
+  },
+  duration: 2,
+  backgroundColor: "#3B2EEF",
+  ease: "none"
+});
+*/
+gsap.to("#pageOne", {
+  scrollTrigger: {
+    trigger: ".one",
+    start: 'top center',
+    end: 'bottom 50%',
+    toggleActions: "play reverse play reverse",
+    scroller: "[data-scroll-container]"
+  },
+  duration: 0.5,
+  color: "#fff",
+  ease: "none"
+});
+
+gsap.to("#pageTwo", {
+  scrollTrigger: {
+    trigger: ".two",
+    start: 'top center',
+    end: 'bottom 50%',
+    toggleActions: "play reverse play reverse",
+    scroller: "[data-scroll-container]"
+  },
+  duration: 0.5,
+  color: "#fff",
+  ease: "none"
+});
+
+gsap.to("#pageThree", {
+  scrollTrigger: {
+    trigger: ".three",
+    start: 'top center',
+    end: 'bottom 50%',
+    toggleActions: "play reverse play reverse",
+    markers: true,
+    scroller: "[data-scroll-container]"
+  },
+  duration: 0.5,
+  color: "#fff",
+  ease: "none"
+});
+
+
+var navigation = new TimelineLite({
+  paused: true,
+  reversed: true
+});
+navigation.to("#navigationWrap", 0.5, {
+    opacity: 1,
+    display: 'block'
+  })
+  .to(".navbar", 0.3, {
+    opacity: 0
+  }, "-=0.1")
+  .to(".close", 0.3, {
+    display: "block",
+    opacity: 1
+  }, "-=0.1")
+  .from(".menu2", 0.5, {
+    opacity: 0,
+    y: 30
+  })
+  .from(".social", 0.5, {
+    opacity: 0
+  });
+
+$(".navbar, .close").click(function () {
+  navigation.reversed() ? navigation.play() : navigation.reverse();
+})
+
+
+
+// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+ScrollTrigger.refresh();
+
+console.log('ya');
