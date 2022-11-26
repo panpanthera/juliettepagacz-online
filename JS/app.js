c9e6f07c72dec358f389ce0b459ed66db1cca135
Juliette Pagacz <jpagacz@student.s19.be>
2022-11-26 15:10:10 +0100

    Gallery slider

--- JS/app.js
+++ JS/app.js
@@ -96,3 +96,13 @@ function injectElementBetweenItems(arr, element) {
 }
 
 animateTitle('.title')
+
+var sliderMain = document.getElementById("slider-main");
+var item = sliderMain.getElementsByClassName("item");
+function next() {
+    sliderMain.append(item[0]);
+}
+
+function prev() {
+    sliderMain.prepend(item[item.length - 1]);
+}
